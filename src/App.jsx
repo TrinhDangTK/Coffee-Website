fad6d03bb7ec81da4fdc520db4e07932b43512f8
Trinh Dang <trinh.dang@techvsi.com>
2026-08-19 17:11:26 +0700

    Update: Add Home section in default App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header"
+import Home from "./components/Home"
 function App(){
   return(
     <section className="coffee-bg">
       <Header/> 
+      <Home/>
    </section>
   )
 }export default App
